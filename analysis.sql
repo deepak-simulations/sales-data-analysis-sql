f3d214e0073c6bb13a1e852c8ab3f94ec6237bd9
Deepak Pant <pantd507@gmail.com>
2026-04-19 21:25:44 +0530

    new update

--- analysis.sql
+++ analysis.sql
@@ -105,3 +105,139 @@ FROM  (
       FROM sales_data
       GROUP BY Product_Category
        );
+
+
+
+## Label categories ( High/Medium/Low)
+SELECT
+Product_Category,
+total_sale,
+
+CASE
+  WHEN total_sale > (
+     SELECT AVG(category_total)
+     FROM (
+       SELECT SUM(Sales_Amount) AS category_total 
+       FROM sales_data
+       GROUP BY Product_Category
+          )
+    ) THEN 'HIGH'
+   ELSE 'LOW'
+  END
+
+FROM(
+  SELECT
+    Product_Category,
+    SUM(Sales_Amount) AS total_sale
+  FROM sales_data
+  GROUP BY Product_Category
+);
+
+
+#top performance region wiese
+
+SELECT
+t.Product_Category,
+t.Region,
+t.total_sales
+
+FROM(
+   SELECT Product_Category,Region,SUM(CAST(Sales_Amount AS REAL)) AS total_sales
+   FROM  sales_data
+   GROUP BY Product_Category,Region
+    ) As t
+WHERE t.total_sales = (
+   SELECT MAX(total_sales)
+   FROM (
+        SELECT Region,Product_Category,SUM(CAST(Sales_Amount AS REAL)) AS total_sales
+        FROM sales_data
+        GROUP BY Region,Product_Category)t2
+        WHERE t2.Region = t.Region
+);
+
+
+# use of window functions 
+
+SELECT
+  Region,
+  Product_Category,
+  total_sales
+
+FROM (
+  SELECT 
+    Region,
+    Product_Category,
+    SUM(Sales_Amount) AS total_sales,
+   
+
+   ROW_NUMBER() OVER(
+     PARTITION BY Region
+     ORDER BY SUM(Sales_Amount) DESC
+   ) AS rank 
+   FROM sales_data
+   GROUP BY Region,Product_Category
+)t
+Where rank= 1;
+
+
+
+# top 2 categories performance wise
+
+SELECT
+  Region,
+  Product_Category,
+  total_sales
+
+FROM (
+  SELECT 
+    Region,
+    Product_Category,
+    SUM(Sales_Amount) AS total_sales,
+   
+
+   ROW_NUMBER() OVER(
+     PARTITION BY Region
+     ORDER BY SUM(Sales_Amount) DESC
+   ) AS rank 
+   FROM sales_data
+   GROUP BY Region,Product_Category
+)t
+Where rank <= 2;
+
+
+# regions wehre the top category constributes more than 50% of total region sales 
+
+
+SElECT 
+Product_Category,
+Region,
+total_sales,
+region_total,
+ROUND((total_sales/region_total)*100,2) AS contribution
+
+FROM(
+SELECT
+   Product_Category,
+   Region,
+   total_sales,
+   SUM(total_sales) OVER(PARTITION BY Region) AS region_total,
+
+
+    ROW_NUMBER() OVER(
+        PARTITION BY Region
+        ORDER BY total_sales DESC
+       ) AS rank
+
+
+FROM(
+   SELECT
+      SUM(Sales_Amount) AS total_sales,
+      Product_Category,
+      Region
+
+
+   FROM sales_data
+   GROUP BY Product_Category, Region
+ ));
+
+
